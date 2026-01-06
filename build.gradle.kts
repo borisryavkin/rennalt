821f65338121c996a26d1d25cde5845d05665b7b
borisry <borisry@icloud.com>
2026-01-06 19:34:52 +1000

    first upload of ios and android app
    not much, but honest work for which I got payed. First I did the android app where I trouble shot the ui then did the ios app

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    kotlin("multiplatform") version "1.9.22" apply false
-    kotlin("plugin.serialization") version "1.9.22" apply false
-    id("com.android.application") version "8.2.2" apply false
-    id("com.android.library") version "8.2.2" apply false
-}
